297d5a799bb4da83ac335936f8a8ca82c411d526
Tiburones202 <tdsfanworksEs@gmail.com>
2023-05-18 17:57:00 +0200

    kinda fixed the health bar thing? idk

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -4914,8 +4914,8 @@ class PlayState extends MusicBeatState
 		
 		switch (uiType){
 			default:
-				iconP1.x = healthBar.x + (healthBar.width) * (FlxMath.remapToRange(healthBar.percent, 0, 100, 100, 0) * 0.01) + (150 * iconP1.scale.x - 150) / 2 - iconOffset;
-				iconP2.x = healthBar.x + (healthBar.width) * (FlxMath.remapToRange(healthBar.percent, 0, 100, 100, 0) * 0.01) - (150 * iconP2.scale.x) / 2 - iconOffset * 2;
+				iconP1.x = (healthBar.x + 80) + ((healthBar.width - 160) * FlxMath.remapToRange(healthBar.percent, 0, 100, 100, 0) * 0.01) + (150 * iconP1.scale.x - 150) / 2 - iconOffset;
+				iconP2.x = (healthBar.x + 80) + ((healthBar.width - 160) * FlxMath.remapToRange(healthBar.percent, 0, 100, 100, 0) * 0.01) - (150 * iconP2.scale.x) / 2 - iconOffset * 2;
 		}
 
 		if (iconP3 != null) 
