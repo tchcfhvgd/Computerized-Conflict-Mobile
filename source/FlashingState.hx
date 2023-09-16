3594dd213c19e3bf8f617f74725ec91d46cdf898
MijaeLio <87155057+MijaeLio@users.noreply.github.com>
2023-09-16 13:51:21 -0500

    sd

--- source/FlashingState.hx
+++ source/FlashingState.hx
@@ -72,7 +72,7 @@ class FlashingState extends MusicBeatState
 		spikes2.scrollFactor.set(0, 0);
 		if (spikes2 != null) add(spikes2);
 		
-		var flashText = new FlxText(250, 125, FlxG.width, "Shaders, Screen Shake and Flashing Lights\nare enabled by default.\n(There's are also advanced shaders which\nmight be too laggy)", 42);
+		var flashText = new FlxText(250, 125, FlxG.width, "Shaders, Screen Shake and Flashing Lights\nare enabled by default.\n(There are also advanced shaders which\nmight be too laggy)", 42);
 		flashText.setFormat(Paths.font("phantommuff.ttf"), 30, FlxColor.WHITE, CENTER, FlxTextBorderStyle.OUTLINE, FlxColor.TRANSPARENT);
 		add(flashText);
 		
